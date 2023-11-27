78473e2e3e66e3f38c3b4ec9850e156561df5fd9
soujanya2308 <117831336+soujanya2308@users.noreply.github.com>
2023-11-27 11:55:39 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int score = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        do {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("\nI've selected a number between " + lowerBound + " and " + upperBound + ". Can you guess it?");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    guessedCorrectly = true;
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Its too low! Try another number .");
+                } else {
+                    System.out.println("Its too high! Try different number.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The correct number was: " + targetNumber);
+            } else {
+                score++;
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+        } while (scanner.next().equalsIgnoreCase("yes"));
+
+        System.out.println("Thank you for playing! Your total score is: " + score);
+        scanner.close();
+    }
+}
